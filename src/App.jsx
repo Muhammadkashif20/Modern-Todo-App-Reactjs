279f5a3dc25ab66f36ee7188eeee7454999b1cef
muhammadkashif20 <kashif28@gmail.com>
2024-10-27 18:23:05 +0500

    added user complete move to delete user

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,24 @@ const TodoApp = () => {
   const [inputValue,setInputValue]=useState('')
   const [todo,addTodo]=useState([])
   const handleTodoValue=()=>{   
-    console.log(todo);
-    addTodo([...todo,inputValue])
-    setInputValue('')  
+    if(inputValue.trim()==''){
+      alert('Please Enter Your Input Value 🙄')
+    }
+    else{
+      console.log(todo);
+      addTodo([...todo,inputValue])
+      setInputValue('')  
+    }
+  }
+  const handleDeleteFunctionality=(id)=>{
+    const removeTodoFromTodoList=todo.filter((text,index)=>index!==id)
+    addTodo(removeTodoFromTodoList)
+    console.log(removeTodoFromTodoList);
   }
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white shadow-md rounded-lg p-8 w-full max-w-md">
-        <h1 className="text-3xl font-bold text-center text-gray-800">To-Do List</h1>
+      <div className="bg-white shadow-md rounded-lg p-8 w-full max-w-md" >
+        <h1 className="text-3xl font-bold text-center text-gray-800">To-Do List 📃</h1>
         <div className="mt-6">
           <input
             type="text"
@@ -26,17 +36,20 @@ const TodoApp = () => {
           Add
         </button>
         </div>
-          {/* {todo.map((userText,userId)=>{
-                <ul className="mt-4 space-y-3">
+          {todo.map((userText,userId)=>{
+            return(
+                <ul className="mt-4 space-y-3" key={userId}>
                 <li className="flex justify-between items-center bg-gray-50 p-3 rounded-md shadow">
-                  <span className="text-gray-700">{userText}</span>
+                  <span className="text-gray-700"  >{userText}</span>
                   <button className="text-red-500 hover:text-red-700 text-[1.20rem]"
+                  onClick={handleDeleteFunctionality}
                   >
                     &#10005;
                   </button>
                 </li>
               </ul>
-              })} */}
+            )
+              })}
      
       </div>
     </div>
